986762ac2d63a3cd2b5870ecc38cf04b62e09860
jasim-n <muhammadsr972@gmail.com>
2021-03-23 23:48:31 +0500

    Initial commit

---
+++ myjscode.js
@@ -0,0 +1,51 @@
+$(function () {
+
+    $("#myForm").submit(handleFormSubmit);
+  });
+  
+  function handleFormSubmit(e) {
+    var name = $("#name").val();
+    var password = $("#password").val();
+    var confirm_password = $("#confirm_password").val();
+    var nameWarning=$("#nameWarning");
+
+    if (!name.length) {
+        $("#name").addClass("error");
+        nameWarning.html("Name Error");
+        nameWarning.css("visibility" , "visible");
+        e.preventDefault();
+        
+    }else{
+    if (name.length<8) {
+        $("#name").addClass("error");
+        nameWarning.html("name length atleast be 8 charachter long");
+        nameWarning.css("visibility" , "visible");
+        e.preventDefault();
+        
+    }else{
+        $("#name").removeClass("error");
+        nameWarning.css("visibility" , "hidden");
+    }}
+    if (password.length < 8) {
+        $("#password").addClass("error");
+        $("#passwordWarning").html("password length should be least 8 characters ");
+        $("#passwordWarning").css("visibility" , "visible");
+
+        e.preventDefault();
+        
+    }else $("#passwordWarning").css("visibility" , "hidden");
+
+    if  ((password!=confirm_password) || !(confirm_password.length)){
+        $("#confirmWarning").html("password dont match");
+            $("#confirm_password").addClass("error");
+            $("#confirmWarning").css("visibility" , "visible");
+            e.preventDefault();
+           
+        }else {
+            $("#confirmWarning").css("visibility" , "hidden");
+            $("#confirm_password").removeClass("error");}
+            e.preventDefault();
+    
+}
+
+  
